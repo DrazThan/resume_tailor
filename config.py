29f916e26024e6b4031d46caeb70c3fb3d6a0b3a
DrazThan <oz.tal000@gmail.com>
2024-08-21 14:53:31 +0300

    print out env file location for debugging

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+print(f"Config file location: {os.path.abspath(__file__)}")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
